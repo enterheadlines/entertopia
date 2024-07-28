30a5d271ce7157b9473562004faf20cca2349082
enterheadlines <162852023+enterheadlines@users.noreply.github.com>
2024-07-28 22:27:45 +0900

    assddsaasddsadasdsadsadsa

---
+++ js/fullscreen.js
@@ -0,0 +1,13 @@
+function toggleFullscreen() {
+    let elem = document.querySelector("iframe");
+  
+    if (!document.fullscreenElement) {
+      elem.requestFullscreen().catch((err) => {
+        alert(
+          `Error attempting to enable fullscreen mode: ${err.message} (${err.name})`,
+        );
+      });
+    } else {
+      document.exitFullscreen();
+    }
+  }
