dca435dee75ada898e5337a2aa127b37c5463f38
enterheadlines <162852023+enterheadlines@users.noreply.github.com>
2024-08-19 10:29:13 +0900

    dsadsaad

--- js/time.js
+++ js/time.js
@@ -18,19 +18,3 @@ function timeFunction() {
 	document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds + ampm ;
 }
 setInterval(timeFunction, 1000);
-
-javascript:(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { eruda.init(); } })();
-;(function () {
-    var src = '//cdn.jsdelivr.net/npm/eruda';
-    if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
-    document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
-    document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
-})();
-
-let el = document.createElement('div');
-document.body.appendChild(el);
-
-eruda.init({
-    container: el,
-    tool: ['console', 'elements']
-});
