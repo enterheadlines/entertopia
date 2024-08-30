a94742f9d232cc84ecdf0b4aaf981649a05759ef
infamy <162852023+enterheadlines@users.noreply.github.com>
2024-08-30 20:55:21 +0900

    dasdsa

--- js/search.js
+++ js/search.js
@@ -2,6 +2,6 @@
 function search() {
     const input = document.getElementById("search").value.toLowerCase();
     document.querySelectorAll('.row').forEach(dock => 
-    dock.style.display = (dock.querySelector('.dock')?.innerText.toLowerCase().includes(input)) ? "" : "none"
+    dock.style.display = (dock.querySelector('#hide')?.innerText.toLowerCase().includes(input)) ? "" : "none"
     );
     }
