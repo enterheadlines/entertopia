b67ac8bd248c09123ac641f098e9b61d9f35e766
infamy <162852023+enterheadlines@users.noreply.github.com>
2024-08-30 20:47:18 +0900

    dsads

--- js/search.js
+++ js/search.js
@@ -1,14 +1,7 @@
-function filterNames() {
-    const input = document.getElementById('search');
-    const filter = input.value.toLowerCase();
-    const names = document.getElementsByClassName('dock');
-    
-    for (let i = 0; i < names.length; i++) {
-        const name = names[i].getAttribute('data-name').toLowerCase();
-        if (name.includes(filter)) {
-            names[i].style.display = ""; // Show the div
-        } else {
-            names[i].style.display = "none"; // Hide the div
-        }
+
+function search() {
+    const input = document.getElementById("search").value.toLowerCase();
+    document.querySelectorAll('.dock').forEach(dock => 
+    dock.style.display = (dock.querySelector('div')?.innerText.toLowerCase().includes(input)) ? "" : "none"
+    );
     }
-}
