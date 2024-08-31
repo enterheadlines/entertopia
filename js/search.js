e21a29bc848b2309b522146494eefcbd084ebc15
infamy <162852023+enterheadlines@users.noreply.github.com>
2024-08-31 11:20:25 +0900

    cvgbhj

--- js/search.js
+++ js/search.js
@@ -1,11 +1,9 @@
-  // Wait for the DOM to fully load
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const categorySelect = document.getElementById('categorySelect');
 
-    // Function to filter games based on dropdown selection
     function filterGames() {
         const selectedCategory = categorySelect.value;
-        const games = document.querySelectorAll('.container > .row');
+        const games = document.querySelectorAll('.row > .column');
 
         games.forEach(game => {
             if (selectedCategory === "" || game.getAttribute('data-category') === selectedCategory) {
@@ -16,6 +14,5 @@
         });
     }
 
-    // Setting up event listener for dropdown changes
     categorySelect.addEventListener('change', filterGames);
 });
